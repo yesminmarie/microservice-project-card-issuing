2456d48fa242c8e5d9aab2b4313c9698e8f26548
Yesmin Lahoud <ymslahoud@gmail.com>
2022-12-16 17:06:23 -0300

    log de erro ao receber solicitação de emissão de cartão

--- mscartoes/src/main/java/io/github/yesminmarie/mscartoes/infra/mqueue/EmissaoCartaoSubscriber.java
+++ mscartoes/src/main/java/io/github/yesminmarie/mscartoes/infra/mqueue/EmissaoCartaoSubscriber.java
@@ -8,12 +8,14 @@ import io.github.yesminmarie.mscartoes.domain.DadosSolicitacaoEmissaoCartao;
 import io.github.yesminmarie.mscartoes.infra.repository.CartaoRepository;
 import io.github.yesminmarie.mscartoes.infra.repository.ClienteCartaoRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class EmissaoCartaoSubscriber {
 
     private final CartaoRepository cartaoRepository;
@@ -32,7 +34,7 @@ public class EmissaoCartaoSubscriber {
 
             clienteCartaoRepository.save(clienteCartao);
         }catch (Exception e){
-            e.printStackTrace();
+            log.error("Erro ao receber solicitação de emissão de cartão: {} ", e.getMessage());
         }
 
     }
